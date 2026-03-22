3a0ee4aa7be5680d81ce98232dcef6815f25b696
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2026-03-22 02:14:59 +0000

    ✨ FEAT: Update home view Update home view model title and enhance layout with navigation and main content

--- server/modules/home/home.service.js
+++ server/modules/home/home.service.js
@@ -1,3 +1,3 @@
 export function getHomeViewModel() {
-  return { title: "Express" };
+  return { title: "✍️ ProjNotes" };
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // Punto de entrada principal para Vite
 // Importar estilos
 import "./styles/main.css";
-import "./styles/header.css";
 
 // Tu código JavaScript aquí
 console.log("🎉 Estilos cargados correctamente 🎉");
