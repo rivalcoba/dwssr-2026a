4d77757f2eb52c747d233ccc55dc943f4137ddef
rivalcoba <ivan.rivalcoba@gmail.com>
2026-03-25 16:02:00 +0000

    ♻️ Refactor: Set up Express server with routing and views, add author and error handling

--- server/app.js
+++ server/app.js
@@ -24,8 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-console.log("Ruta:  " + path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname,'..', 'public')));
 
 // Registrando las rutas a los enrutadores
 app.use(['/','/index'], indexRouter);
