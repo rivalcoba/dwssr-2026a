ac48e2f41aebd21b167c0d314af8d6193e81e4ed
rivalcoba <ivan.rivalcoba@gmail.com>
2026-03-25 16:56:04 +0000

    🔧 chore: Vit configuration En progeso...

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// Importo la funcion de configuracion de Vite
+import { defineConfig } from 'vite'
+// Importo un resolvedor de rutas
+import { resolve } from "node:path"
+
+// Exportar una instancia de configuracion
+export default defineConfig({})
