902978e7002e0a535e0c704602750443ac15eaaf
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2026-04-24 16:56:26 +0000

    ✨chore: add ESLint configuration and scripts for linting
    - Updated package.json to include linting scripts for backend and frontend.
    - Added ESLint and Prettier as devDependencies.
    - Created eslint.config.js to define ESLint rules for server and src directories.
    - Added eslint-disable comments for unused variables in route handlers.

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 /* GET users listing. */
+// eslint-disable-next-line no-unused-vars
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
