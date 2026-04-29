a0ac6dfa925510c68e6398cfab63dfcd5959dc75
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2026-04-29 16:52:59 +0000

    ✨ feat: CSS Framework Enhance project setup with Tailwind CSS integration, update scripts, and add header styles

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@
 import { defineConfig } from 'vite'
 // Importo un resolvedor de rutas
 import { resolve } from "node:path"
+// Importando Tailwind
+import tailwindcss from "@tailwindcss/vite";
 
 // Exportar una instancia de configuracion
 export default defineConfig({
   // Directorio raiz de los archivos fuente
   root: 'src',
+  plugins: [
+    tailwindcss(),
+  ],
   // Configuracion del servidor
   // de desarrollo de front-end
   server: {
